37dd07768bb862ff5087c144c9f33b45875e9139
Clément de Bailliencourt <clementdebailliencourt@gmail.com>
2018-03-29 16:01:25 +0200

    Création du menu et connexion aux bdd

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,21 +4,89 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static char menu() {
 
+        System.out.println("Bienvenue dans le projet Centre Hospitalier ! \n");
+
+        String choix;
+        System.out.println("1 Pour une connexion en SSH");
+        System.out.println("2 Pour une connexion en localhost");
+        System.out.println("\n0 Quitter");
+
+        Scanner sc = new Scanner(System.in);
+        choix = sc.next();
+
+        return choix.charAt(0);
+    }
+
+    public static void connexionSSH(){
         //Connexion serveur
         Scanner saisieClavier = new Scanner(System.in);
 
-        System.out.println("Bienvenue dans le projet Centre Hospitalier ! \n\nVeuillez entrer votre identifiant :");
-        String username = saisieClavier.nextLine();
+        System.out.println("\nVeuillez entrer votre identifiant ECE :");
+        String usernameECE = saisieClavier.nextLine();
 
-        System.out.println("\n\nVotre mot de passe :");
-        String password = saisieClavier.nextLine();
-        System.out.println("\n\nlol :");
-        SSHTunnel TestConnection = new SSHTunnel(username, password);
+        System.out.println("\nVotre mot de passe ECE :");
+        String passwordECE = saisieClavier.nextLine();
 
-        //Test des données récupérées
-        System.out.println("\n\n" + TestConnection.getUsername() + "\n\n" + TestConnection.getFirstHostPort() + "\n\n" + TestConnection.getSecondHostPort() );
+        System.out.println("\nVotre identifiant SQL:");
+        String usernameSQL = saisieClavier.nextLine();
 
+        System.out.println("\nVotre mot de passe SQL:");
+        String passwordSQL = saisieClavier.nextLine();
+
+        try {
+            Connexion connectSQL = new Connexion(usernameECE, passwordECE, usernameSQL, passwordSQL);
+        } catch (Exception  ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void connexionLocalhost(){
+
+        Scanner saisieClavier = new Scanner(System.in);
+
+        System.out.println("\nVeuilliez entrer le nom de votre base de donnée :");
+        String nameDatabase = saisieClavier.nextLine();
+
+        System.out.println("\nVotre identifiant SQL :");
+        String loginDatabase = saisieClavier.nextLine();
+
+        System.out.println("\nVotre mot de passe SQL:");
+        String passwordDatabase = saisieClavier.nextLine();
+
+        try {
+            Connexion connectSQL = new Connexion(nameDatabase, loginDatabase, passwordDatabase);
+        } catch (Exception  ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        boolean run = true;
+
+        do {
+            char choice = menu();
+
+            switch (choice) {
+                case '1': // Connection en SSH
+                    connexionSSH();
+                    run = false;
+                    break;
+
+                case '2': // Connection en localhost
+                    connexionLocalhost();
+                    run = false;
+                    break;
+
+                case '0':
+                    run = false;
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Erreur de choix");
+            }
+        } while (run);
     }
 }
