8a42dd069033e0a2c4ec0f3db98df46378586b19
MYRNA\Myrna <myrna.el-awady@edu.ece.fr>
2018-03-30 12:46:45 +0200

    good

--- src/Main/Graphisme.java
+++ src/Main/Graphisme.java
@@ -3,24 +3,56 @@ package Main;
 import javax.swing.*;
 
 public class Graphisme extends JFrame {
+
     private JPanel pan; // panneau
+    private JButton buttonSSH, buttonLocalhost;
+    private JLabel welcomeL1, welcomeL2;
     private ImageIcon hopital;
     private boolean created;
 
+
 private JLabel image;
+
+  //  private JLabel image3;
+
+
     public Graphisme()
     {
         setTitle("Gestion d'un centre hospitalier");
         setSize(800, 600);
         setLocation(425, 200);
         pan = new JPanel(); // instancier le panneau
-        //  hopital = new ImageIcon("/Annexe/hopital.png");
         image = new JLabel(new ImageIcon("hopital.jpg"));
-        add(image);
 
-        //getContentPane().add(pan);
+        welcomeL1 = new JLabel();
+        welcomeL1.setLocation(200,60);
+        welcomeL1.setText("Bienvenue dans le système de gestion de votre Centre Hospitalier");
+        welcomeL1.setSize(500,20);
+
+        welcomeL2 = new JLabel();
+        welcomeL2.setLocation(260,80);
+        welcomeL2.setText("Veuilliez choisir le type de connexion souhaitée :");
+        welcomeL2.setSize(500,20);
+
+        buttonSSH = new JButton();
+        buttonSSH.setLocation(250, 500);
+        buttonSSH.setText("SSH");
+        buttonSSH.setSize(80,20);
+
+        buttonLocalhost = new JButton();
+        buttonLocalhost.setLocation(450, 500);
+        buttonLocalhost.setText("Localhost");
+        buttonLocalhost.setSize(80,20);
+
+        //created = true;
+    }
+    public void afficherMenu(){
+        this.add(welcomeL1);
+        this.add(welcomeL2);
+        this.add(buttonSSH);
+        this.add(buttonLocalhost);
+        this.add(image);
+
         this.setVisible(true);
-
-        created = true;
     }
 }
